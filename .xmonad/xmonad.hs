8fd295fbc054169bf2d6ee52ee0d8362a5dae8fc
Mindaugas Celiesius <m.celiesius@yandex.ru>
2019-03-11 20:24:34 +0200

    Added some scripts, custom layouts etc...

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,23 +3,57 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.BinarySpacePartition (emptyBSP)
+import XMonad.Layout.Maximize
+import XMonad.Layout.Grid
 import XMonad.Layout.NoBorders
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.Column
+import XMonad.Util.EZConfig
 import System.IO
-
+import XMonad.Prompt
+import XMonad.Prompt.ConfirmPrompt
+import XMonad.Actions.WindowBringer
+import XMonad.Prompt.Shell
+import System.Exit
 
 main = do
   xmproc <- spawnPipe "/usr/bin/xmobar /home/mindaugas/.xmobarrc"
   xmonad $ ewmh def
-    { borderWidth = 3
-    , terminal = "urxvtc"
+    { terminal = "urxvtc"
     , modMask = mod4Mask
-    , normalBorderColor = "#111111"
-    , focusedBorderColor = "#444488"
     , manageHook = manageDocks 
-    , layoutHook = avoidStruts $ smartBorders $ layoutHook def
+    , layoutHook = avoidStruts $ smartBorders $ myLayouts
     , handleEventHook = fullscreenEventHook <+> docksEventHook
-    , logHook = dynamicLogWithPP def
+    , logHook = dynamicLogWithPP xmobarPP
                 { ppOutput = hPutStrLn xmproc
+                , ppTitle = xmobarColor "green" "" . shorten 50
                 }
     }
+
+     `additionalKeysP` -- Add some extra key bindings:
+      [ 
+        ("M-S-q",   confirmPrompt myXPConfig "exit" (io exitSuccess))
+      , ("M-p",     shellPrompt myXPConfig)
+      , ("M-<Esc>", withFocused (sendMessage . maximizeRestore))
+      , ("M-S-g",   gotoMenu)
+      , ("M-S-b",   bringMenu)
+      , ("M-w", spawn "/home/mindaugas/.scripts/screeny")
+      , ("M-r", spawn "/home/mindaugas/.scripts/shutdown.sh")
+      , ("M-x", spawn "/home/mindaugas/.scripts/mpdmenu")
+      ]
+--------------------------------------------------------------------------------
+-- | Customize layouts.
+--
+-- This layout configuration uses two primary layouts, 'ResizableTall'
+-- and 'BinarySpacePartition'.  You can also use the 'M-<Esc>' key
+-- binding defined above to toggle between the current layout and a
+-- full screen layout.
+myLayouts = maximizeWithPadding 10 (Tall 1 (3/100) (1/2)) ||| emptyBSP ||| Column 1.6
+
+myXPConfig = def
+  { position          = Top
+  , alwaysHighlight   = True
+  , promptBorderWidth = 0
+  , font              = "xft:monospace:size=9"
+  }
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-It is basic Xmonad config with xmobar, a good starting point for more complex setups.
+It is Xmonad config with xmobar, a good starting point for more complex setups. I just added custom layouts (Maximize Tall, BSP, Column), some usefull scripts (in .scripts folder), custom prompts etc.
 
 Screenshots
 
 ![Screenshot](screen.png?raw=true "Clear")
 ![Screenshot](screen_1.png?raw=true "Terminals")
 ![Screenshot](screen_2.png?raw=true "Ranger")
+![Screenshot](screen_3.png?raw=true "mpd")
+![Screenshot](screen_4.png?raw=true "windows")
 
